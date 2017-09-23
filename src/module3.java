1f215b0821c54fb0457f43cac7282f005a5d908a
dvhaldane <dvhaldane@me.com>
2017-09-22 20:26:12 -0500

    first commit

---
+++ src/module3.java
@@ -0,0 +1,66 @@
+/**
+ * Created by HaldaneDavidV on 9/22/2017.
+ */
+public class module3
+{
+    public static void main(String[] args)
+    {
+        Comparable[] array = {1, 2, 2, 2, 4, 5, 8, 9, 14, 32};
+        //Comparable[] array = {"APPLE", "BANANA", "BANANA", "BANANA", "BLUEBERRY", "COCONUT", "DURAN"};
+
+       System.out.println(binarySearchForInsert(array, 15));
+    }
+
+    public static int binarySearchForInsert(Comparable[] objArray, Comparable searchObj)  //Will return index of found item.  If more than one of item returns index of last item.
+    {
+        int low = 0;
+        int high = objArray.length - 1;
+        int mid = 0;
+
+        //Handle empty array
+        if (objArray.length == 0)
+        {
+            return 0;
+        }
+
+        while (low <= high)
+        {
+            mid = (low + high) / 2;
+            if (objArray[mid].compareTo(searchObj) < 0)
+            {
+                low = mid + 1;
+            }
+            else if (objArray[mid].compareTo(searchObj) > 0)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+
+                int prestep = mid;
+                int poststep = mid + 1;
+
+                //Run loop to find end of repeating values
+                while (objArray[prestep].compareTo(objArray[poststep]) == 0)
+                {
+                    mid = poststep;
+                    prestep++;
+                    poststep++;
+                }
+
+                //Will return index to insert which is found index + 1
+                return mid + 1;
+            }
+        }
+
+        //Handle insertion of data not already in the array
+
+        System.out.println("Low is " + low);
+        System.out.println("High is " + high);
+        System.out.println("Mid is" + mid);
+
+        return mid + 1;  //Return fail code which is length + 1;
+    }
+}
+
+
---
+++ src/module3Test.java
@@ -0,0 +1,111 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by HaldaneDavidV on 9/22/2017.
+ */
+public class module3Test extends module3
+{
+    @Test
+    public void testEmptyArray() throws Exception
+    {
+        Comparable[] array = {};
+
+        Assert.assertTrue(binarySearchForInsert(array, 2) == 0);
+    }
+
+    @Test
+    public void testLowerBoundExistsOdd() throws Exception
+    {
+        Comparable[] array = {2,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 2) == 1);
+    }
+
+    @Test
+    public void testLowerBoundExistsEven() throws Exception
+    {
+        Comparable[] array = {2,3,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 2) == 1);
+    }
+
+    @Test
+    public void testLowerBoundNotExistsOdd() throws Exception
+    {
+        Comparable[] array = {2,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 1) == 4);
+    }
+
+    @Test
+    public void testLowerBoundNotExistsEven() throws Exception
+    {
+        Comparable[] array = {2,3,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 1) == 5);
+    }
+
+    @Test
+    public void testUpperBoundExistsOdd() throws Exception
+    {
+        Comparable[] array = {2,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 4) == 3);
+    }
+
+    @Test
+    public void testUpperBoundExistsEven() throws Exception
+    {
+        Comparable[] array = {2,3,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 4) == 4);
+    }
+
+    @Test
+    public void testUpperBoundNotExistsOdd() throws Exception
+    {
+        Comparable[] array = {2,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 5) == 4);
+    }
+
+    @Test
+    public void testUpperBoundNotExistsEven() throws Exception
+    {
+        Comparable[] array = {2,3,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 5) == 5);
+    }
+
+    @Test
+    public void testMiddleBoundExistsOdd() throws Exception
+    {
+        Comparable[] array = {2,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 3) == 2);
+    }
+
+    @Test
+    public void testMiddleBoundExistsEven() throws Exception
+    {
+        Comparable[] array = {2,3,3,4};
+
+        Assert.assertTrue(binarySearchForInsert(array, 3) == 3);
+    }
+
+    @Test
+    public void testMiddleBoundNotExistsOdd() throws Exception
+    {
+        Comparable[] array = {2,4,4,5};
+
+        Assert.assertTrue(binarySearchForInsert(array, 3) == 1);
+    }
+
+    @Test
+    public void testMiddleBoundNotExistsEven() throws Exception
+    {
+
+    }
+
+}
